6e539c02a65d7fd08a5d85a9afa3cda4a9f3747a
Christina Lupanow <celupanow@gmail.com>
2020-03-16 22:04:44 -0600

    updating a few files with basic code

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+// The player must choose a character - there are four options. Then the player must choose an opponent from the remaining options.
+
+// Possibly it would make sense to create an object for each character?
+
+// var Person1 {
+// 	name: Joe Schmoe;
+// 	picture: ??;
+// 	health-points: 150;
+// 	attack-power: 60;
+// 	counter-attack-power: 20;
+// };
+
+// Like this maybe? Do this for each character.
+
+// So then there will be an onclick event for the player to choose their fighter.
+// After which, the rest of the characters become enemies and move to a different part of the screen.
+// Then another onclick event for the player to choose their opponent, which moves them to a defender area.
+// Then there will be an attack button.
+// Attack button: enemy loses hp, then instantly counterattacks, causing player to lose some hp.
+// Defender gets to zero, player chooses a new opponent.
+// if the player gets to zero, they lose, if they defeat all enemies, they win
+
+
+
